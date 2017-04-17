ff808504d2e04c3346b1a23ff07e47bcc2bde41f
bosstb <tangbu@foxmail.com>
2017-04-17 23:44:41 +0800

    123

--- 12.py
+++ 12.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import random
 import xml.dom.minidom
+import json
 import os
 import time
 import requests
@@ -14,7 +15,7 @@ from selenium import webdriver
 l=0.1
 l = random.randint(1, 100)
 l=float(l)/100
-class RealtimeScore(leancloud.Object):
+class PushRecord(leancloud.Object):
     pass
 
 
@@ -51,26 +52,66 @@ def scoreDedicate():
     # wait = WebDriverWait(browser, 10)
     a = 1
     while 1 == 1:
-        r = requests.get("http://secim.haberler.com/2017/referandum/")
-        # driver = webdriver.PhantomJS(executable_path='/bin/phantomjs/bin/phantomjs')  # 如果不方便配置环境变量。就使用phantomjs的绝对路径也可以
-        # driver.get('http://image.baidu.com/i?ie=utf-8&word=%E5%91%A8%E6%9D%B0%E4%BC%A6')  # 抓取了百度图片，query：周杰伦
-          # 这就是返回的页面内容了，与urllib2.urlopen().read()的效果是类似的，但比urllib2强在能抓取到动态渲染后的内容。
-        # time.sleep(20)
-        # lis = browser.find_element_by_xpath('/html/body/div[6]/header/div[1]/div/div[2]/div/div[4]/div')
-        doc = etree.HTML(r.text)
-        dd = doc.xpath('//*[@id="mainChart"]/div/div/div[1]/div[2]/span[3]/text()')
-        print dd
-        Todo = leancloud.Object.extend('RealtimeScore')
-        todo = Todo.create_without_data('58ef1b27827459005245ade6')
-        # 这里修改 location 的值
-        todo.set('rightOption', dd[0].replace(',', '.'))
-        # a = a + 1
-        # todo.set('rightOption', str(a))
-        dd = doc.xpath('//*[@id="mainChart"]/div/div/div[1]/div[1]/span[2]/text()')
-        print dd
-        todo.set('leftOption', dd[0].replace(',', '.'))
-        todo.save()
-        time.sleep(30)
-        # driver.quit()
+        # r = requests.get("http://secim.haberler.com/2017/referandum/")
+        # # driver = webdriver.PhantomJS(executable_path='/bin/phantomjs/bin/phantomjs')  # 如果不方便配置环境变量。就使用phantomjs的绝对路径也可以
+        # # driver.get('http://image.baidu.com/i?ie=utf-8&word=%E5%91%A8%E6%9D%B0%E4%BC%A6')  # 抓取了百度图片，query：周杰伦
+        #   # 这就是返回的页面内容了，与urllib2.urlopen().read()的效果是类似的，但比urllib2强在能抓取到动态渲染后的内容。
+        # # time.sleep(20)
+        # # lis = browser.find_element_by_xpath('/html/body/div[6]/header/div[1]/div/div[2]/div/div[4]/div')
+
+
+        f = open("HTML", "r")
+        sql = ''
+        while True:
+            line = f.readline()
+            if line:
+                # pass  # do something here
+                line = line.strip()
+                p = line.rfind('.')
+                filename = line[0:p]
+                sql = sql + ' ' + line
+            else:
+                break
+        f.close()
+        root = etree.HTML(sql)
+        print root
+        doc = etree.HTML(sql)
+        htmls = doc.xpath('/html')
+        edata = []
+        for html in htmls:
+            pushTime = html.xpath('//*[@id="base-main"]/div[2]/div/div/div[3]/div[2]/div/div[1]/ul/li[1]')
+            # data = doc.xpath('//*[@id="base-main"]/div[2]/div/div/div[3]/div[2]/div[3]/div[1]/ul/li[1]')
+            targetUser = html.xpath('//*[@id="base-main"]/div[2]/div/div/div[3]/div[2]/div/div[1]/ul/li[4]/span/span[1]')
+            arriveUser = html.xpath('//*[@id="base-main"]/div[2]/div/div/div[3]/div[2]/div/div[1]/ul/li[4]/span/span[3]')
+            clickUser = html.xpath('//*[@id="base-main"]/div[2]/div/div/div[3]/div[2]/div/div[2]/div[1]/ul/li[4]/span[1]')
+            a = 0
+            for item in pushTime:
+                pushContent = html.xpath('//*[@id="notify' + str((a+50) % 5) + '"]/text()')
+                content = json.loads(pushContent[0])
+                ed = (item.text, arriveUser[a].text, targetUser[a].text, clickUser[a].text, content.get("ios").get("extras").get("news_id"), content.get("ios").get("alert"))
+                edata.append(ed)
+                a += 1
+        import xlwt
+        workbook = xlwt.Workbook(encoding='utf-8')
+        booksheet = workbook.add_sheet('Sheet 1', cell_overwrite_ok=True)
+        for i, row in enumerate(edata):
+            for j, col in enumerate(row):
+                booksheet.write(i, j, col)
+        workbook.save('d:\grade.xls')
+
+
+
+
+        return 1
+        # todo = PushRecord()
+        # todo.set('rightOption', dd[0].replace(',', '.'))
+        # # a = a + 1
+        # # todo.set('rightOption', str(a))
+        # dd = doc.xpath('//*[@id="mainChart"]/div/div/div[1]/div[1]/span[2]/text()')
+        # print dd
+        # todo.set('leftOption', dd[0].replace(',', '.'))
+        # todo.save()
+        # time.sleep(30)
+        # # driver.quit()
 
 scoreDedicate()
